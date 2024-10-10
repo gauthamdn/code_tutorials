949edeea01851799a76f3800a6a141f88ea11472
Gautham D N <gdn@atlassian.com>
2024-10-10 19:43:12 +0530

    Implementing hash table seperate chaining datastructure

---
+++ src/temp/finbonacci.java
@@ -0,0 +1,26 @@
+package src.temp;
+
+public class finbonacci {
+
+    public static void main(String[] args) {
+
+        int n=150;
+
+        for(int i=0;i<n;i++){
+            System.out.print(fibonacci(i)+" ");
+        }
+
+
+    }
+
+    private static int fibonacci(int n) {
+
+        if(n==0)
+            return 0;
+        else if(n==1 || n==2)
+            return 1;
+        else return fibonacci(n-1) + fibonacci(n-2);
+
+
+    }
+}
